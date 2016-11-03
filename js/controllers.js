a5508c4b5c82c70c8127fb9d1d1c811d752e16db
Sean <lilbrick59@gmail.com>
2016-11-03 21:14:06 +0000

    0.9

---
+++ js/controllers.js
@@ -0,0 +1,30 @@
+angular.module('starter.controllers', [])
+
+.controller('SeatsCtrl', function($scope, $state) {
+  //populating test data for counter
+  $scope.count = 0;
+  $scope.capacity = 40;
+  $scope.plus = function(){
+    if($scope.count>=0 && $scope.count<$scope.capacity){
+      $scope.count++;
+    } else{
+      console.log();
+    }
+  }
+
+  $scope.minus = function(){
+    if($scope.count>0){
+      $scope.count--;
+    } else{
+      console.log();
+    }
+  }
+})
+
+.controller('RouteCtrl', function($scope) {
+
+})
+
+.controller('ContactCtrl', function($scope) {
+
+});

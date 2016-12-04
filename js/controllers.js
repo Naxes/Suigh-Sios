036509a4dcad8eb50a2564dffbf5a91e22546b0c
Seán Bickmore <x14428372@student.ncirl.ie>
2016-12-04 02:58:25 +0000

    BUS FILTER UPDATE

--- js/controllers.js
+++ js/controllers.js
@@ -2,31 +2,32 @@ angular.module('starter.controllers', [])
 
 // Controller for tab-stopNumber
 .controller('StopNumberCtrl', function($scope, $stateParams){
-
+  $scope.matthewsData;
+  
+  var matthewsRef = firebase.database().ref('Matthews Coaches/routes/');
+  matthewsRef.on("value", function(snapshot){
+    console.log(snapshot.val());
+    $scope.matthewsData = snapshot.val();
+    $scope.$evalAsync();
+  });
 })
 
 // Controller for tab-seats
 .controller('SeatsCtrl', function($scope, $state) {
-  $scope.busId;
+  // Global variables for seat count and static data to equal itself when pushing to the database
+  $scope.busNum;
   $scope.count;
   $scope.capacity;
   
-  // Reference first entry in Firebase Database & display data
-  var busRef = firebase.database().ref('buses/0/routes/0/busId/');
-  busRef.on("value", function(snapshot) {
-    console.log(snapshot.val());
-    $scope.busId = snapshot.val();
-    $scope.$evalAsync();
-  });
-  
-  var seatRef = firebase.database().ref('buses/0/routes/0/seats/');
+  // Reference first entry in Firebase Database & display data (Hard coded test)
+  var seatRef = firebase.database().ref('Matthews Coaches/routes/0/seats');
   seatRef.on("value", function(snapshot) {
     console.log(snapshot.val());
     $scope.count = snapshot.val();
     $scope.$evalAsync();
   });
   
-  var capacityRef = firebase.database().ref('buses/0/routes/0/capacity/');
+  var capacityRef = firebase.database().ref('Matthews Coaches/routes/0/capacity/');
   capacityRef.on("value", function(snapshot) {
     console.log(snapshot.val());
     $scope.capacity = snapshot.val();
@@ -34,43 +35,38 @@ angular.module('starter.controllers', [])
   });
   
   // Increment seat count & update database
-  $scope.plus = function(busId, capacity, seats){
+  $scope.plus = function(seats){
     if($scope.count>=0 && $scope.count<$scope.capacity){
       $scope.count++;
     }else{
       console.log();
     }
     
-    firebase.database().ref('buses/0/routes/0/').set({
-        busId: $scope.busId,
-        capacity: $scope.capacity,
-        seats: $scope.count
+    firebase.database().ref('Matthews Coaches/routes/0/').update({
+      seats: $scope.count
     });
   }
   
   // Subtract seat count & update database
-  $scope.minus = function(busId, capacity, seats){
+  $scope.minus = function(seats){
     if($scope.count>0){
       $scope.count--; 
     }else{
       console.log();
     }
     
-    firebase.database().ref('buses/0/routes/0/').set({
-        busId: $scope.busId,
-        capacity: $scope.capacity,
-        seats: $scope.count
-    }); 
+     firebase.database().ref('Matthews Coaches/routes/0/').update({
+      seats: $scope.count
+    });
   }
-
-  $scope.reset = function(busId, capacity, seats){
+  
+  // Reset seat count & update database
+  $scope.reset = function(seats){
     $scope.count = 0;
     
-    firebase.database().ref('buses/0/routes/0/').set({
-        busId: $scope.busId,
-        capacity: $scope.capacity,
-        seats: $scope.count
-    });  
+     firebase.database().ref('Matthews Coaches/routes/0/').update({
+      seats: $scope.count
+    });
   }
 })
 
